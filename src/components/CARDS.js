afec963d6b20b8def981423ee0e83afd905a364d
Gordon Maloney <gordonmaloney@gmail.com>
2021-12-07 16:11:11 +0000

    SR function added - just locally

--- src/components/CARDS.js
+++ src/components/CARDS.js
@@ -1,5 +1,6 @@
 export const CARDS = [
-  { id: 1, level: 1, gd: "sgìth", en: "tired" },
-  { id: 2, level: 1, gd: "beag", en: "small" },
-  { id: 3, level: 1, gd: "mòr", en: "big" },
+  { id: 1, level: 1, gd: "sgìth", en: "tired", date: new Date(), delay: 0 },
+  { id: 2, level: 1, gd: "beag", en: "small", date: new Date(), delay: 0  },
+  { id: 3, level: 1, gd: "mòr", en: "big", date: new Date(), delay: 0  },
+  { id: 4, level: 1, gd: "mi", en: "I, me", date: new Date(), delay: 0  },
 ];
--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -5,70 +5,120 @@ export const Flashcard = () => {
   const [index, setIndex] = useState(0);
   const [answer, setAnswer] = useState(false);
 
-
   const handlePrev = () => {
-      if (index == 0) {
-         setIndex(CARDS.length-1)
-      } else {
-          setIndex(index-1)
-      }
-  }
+    if (index == 0) {
+      setIndex(CARDS.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
 
   const handleNext = () => {
-    if (index == CARDS.length-1) {
-       setIndex(0)
+    if (index == CARDS.length - 1) {
+      setIndex(0);
     } else {
-        setIndex(index+1)
+      setIndex(index + 1);
     }
-}
+  };
 
   return (
-    <div
+    <>
+      <div
         style={{
-            backgroundColor: 'beige',
-            width: "200px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            padding: "25px",
-            marginTop: "100px",
-            border: "1px solid black",
-            borderRadius: "20px"
-
+          backgroundColor: "beige",
+          width: "200px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          padding: "25px",
+          marginTop: "100px",
+          border: "1px solid black",
+          borderRadius: "20px",
         }}
-    >
-      <center>
-        {CARDS[index].en}
-        <br />
-        {answer ? (
-          <>
-            {CARDS[index].gd}
-            <br /><br />
-          </>
-        ) : (
+      >
+        <center>
+          {CARDS[index].en}
+          <br />
+          {answer ? (
             <>
-            <br />
-            <br />
+              {CARDS[index].gd}
+              <br />
+              <br />
             </>
-        )}
-        <button onClick={() => setAnswer(!answer)}>Show Answer</button>
-        <br />
-        <button
-          onClick={() => {
-            setAnswer(false);
-            handlePrev();
-          }}
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => {
-            setAnswer(false);
-            handleNext();
-          }}
-        >
-          Next
-        </button>
-      </center>
-    </div>
+          ) : (
+            <>
+              <br />
+              <br />
+            </>
+          )}
+          <button onClick={() => setAnswer(!answer)}>Show Answer</button>
+          <br />
+          <button
+            onClick={() => {
+              setAnswer(false);
+              handlePrev();
+            }}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => {
+              setAnswer(false);
+              handleNext();
+            }}
+          >
+            Next
+          </button>
+
+          <br />
+          <button
+            onClick={() => {
+              CARDS[index].date.setDate(CARDS[index].date.getDate());
+              CARDS[index].delay = 0;
+              handleNext();
+            }}
+          >
+            Hard
+          </button>
+          <button
+            onClick={() => {
+              CARDS[index].date.setDate(CARDS[index].date.getDate() + 1);
+              CARDS[index].delay = 1;
+              handleNext();
+            }}
+          >
+            Easy
+          </button>
+          <button
+            onClick={() => {
+              CARDS[index].date.setDate(CARDS[index].date.getDate() + 3);
+              CARDS[index].delay = 3;
+              handleNext();
+            }}
+          >
+            Really Easy
+          </button>
+        </center>
+      </div>
+
+      {CARDS.map((card) => (
+        <>
+          {card.gd} - {card.en} - {card.date.toLocaleDateString()} -{" "}
+          {card.delay}
+          <br />
+        </>
+      ))}
+
+<br />
+Due today:
+<br />
+{CARDS.filter(card => card.date <= new Date()).map((card) => (
+        <>
+          {card.gd} - {card.en} - {card.date.toLocaleDateString()} -{" "}
+          {card.delay}
+          <br />
+        </>
+      ))}
+
+    </>
   );
 };
